d9b3b346ed505576eed24272649b33f7be76665b
hassan_sahraoui@yahoo.itgit config --global user.name hassansahraoui@yahoo.it <hassan_sahraoui@yahoo.it>
2023-04-04 09:01:31 +0200

    ultimre mod

---
+++ m1/s2/g1/compito/script.js
@@ -0,0 +1,63 @@
+    
+    var arbitri = 3; 
+
+    if(arbitri == 3){
+     console.log("3")
+    }
+    if(arbitri < 3){
+     console.log('not possible')
+    }
+    if(arbitri > 3){
+     console.log('not possible')
+    }
+
+
+
+
+    var fiat = autoitaliana;
+
+    if(fiat == audi){
+     console.log('auto tedesca')
+    }
+    if(fiat == mercedes){
+     console.log('auto tedesca')
+    }
+    if(fiat == autoitaliana){
+     console.log('auto italiana')
+    }
+    else if(fiat == citroen){
+     console.log('auto francese')
+    }
+
+
+
+//potenzialmente è un pasticcio// 
+
+
+
+
+    var animalePreferito = tigre
+
+    if(animalePreferito == leone){
+     console.log(errato)
+    }
+    else if(animalePreferito == tigre){
+     console.log('corretto')
+    }
+
+
+
+    var giocariPallavolo = 6
+
+    if(giocatoriPallavolo < 6){
+     console.log('beach-volley')
+    }
+    else if(giocatoriPallavolo > 6){
+     console.log('impossible')
+    }
+
+    document.getElementById('ciao mondo')
+    innerhtml = 'ciao mondo'
+    alert('hello word')
+    console.log('hello word')
+    document.write('hello word')
