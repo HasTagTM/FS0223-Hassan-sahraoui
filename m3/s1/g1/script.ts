55afe352ef2d3e35b9d144318dae031747c161d0
hassan_sahraoui@yahoo.itgit config --global user.name hassansahraoui@yahoo.it <hassan_sahraoui@yahoo.it>
2023-05-22 16:17:26 +0200

    giorno 1

---
+++ m3/s1/g1/script.ts
@@ -0,0 +1,20 @@
+let vincitore = document.querySelector('#vincitore')
+const VICTORY:any = function(num1:number , num2:number){
+    let numeroCasuale:number = Math.floor(Math.random() * 99 + 1);
+    let diff1:number = Math.abs(numeroCasuale - num1);
+    let diff2:number = Math.abs(numeroCasuale - num2);
+    
+    if(diff1 === diff2){
+        return `il giocatore 1 ha scelto il numero ${diff1} come il giocatore 2`  
+    }else if(diff1<diff2){
+        return `il giocatore 1 ha scelto il numero ${num1} mentre il giocatore 2 ha scelto il numero ${num2}. 
+        dato che il numero casuale generato è il ${numeroCasuale} assegnamo la vittoria al giocatore 1 che si è avvicinato di più`
+    }else{
+        return `il giocatore 1 ha scelto il numero ${num1} mentre il giocatore 2 ha scelto il numero ${num2}. 
+        dato che il numero casuale generato è il ${numeroCasuale} assegnamo la vittoria al giocatore 2 che si è avvicinato di più`
+    }
+}
+
+
+console.log(VICTORY(26,19));
+ 
