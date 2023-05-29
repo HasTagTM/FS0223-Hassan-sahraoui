2ba2fcaf1ac805fa2e0d7a4e9bb191e68cfcc2fe
hassan_sahraoui@yahoo.itgit config --global user.name hassansahraoui@yahoo.it <hassan_sahraoui@yahoo.it>
2023-05-29 17:02:12 +0200

    giorno 1 angular

---
+++ m3/s2/g1/progetto1/src/app/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+  title = 'progetto1';
+  parag = 'comp2 works!'
+  parag2 = 'comp3 works!'
+}
