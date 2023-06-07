2cdbb7fd6c5e603177a21ecde18a7e86c3ebd6d7
hassan_sahraoui@yahoo.itgit config --global user.name hassansahraoui@yahoo.it <hassan_sahraoui@yahoo.it>
2023-06-07 14:29:36 +0200

    t

--- m3/s3/g2/progetto2/src/app/form1/form1.component.ts
+++ m3/s3/g2/progetto2/src/app/form1/form1.component.ts
@@ -5,21 +5,21 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-form1',
   templateUrl: './form1.component.html',
-  styleUrls: ['./form1.component.css']
+  styleUrls: ['./form1.component.css'],
 })
 export class Form1Component {
+  @ViewChild('f')
+  form!: NgForm
 
-    @ViewChild('f',{static:true}) form!:NgForm
+  constructor(){
 
-    submit(form:NgForm){
-      this.form.reset()
-    }
+  }
+  submit(form: NgForm) {
+    this.form.reset();
+    console.log(form);
+  }
 
-    ngOnInit(){
-      this.form.statusChanges?.subscribe(status =>{
-        console.log(status);
+  ngOnInit(): void {
 
-      })
-    }
-
+  }
 }
